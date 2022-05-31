3fdaaa6b3ea105b89846a02041914624a1284838
aravinddps123 <aravinddps123@gmail.com>
2022-05-31 17:47:26 +0530

    db mock test working

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,10 +2,26 @@
 django command to wait fo db to be available
 
 """
+import time
+
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as psycopg2_OP_error
 
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command for wait fo database"""
     def handle(self, *args, **kwargs):
-        pass
+        """Entry point fo the command """
+        self.stdout.write("waiting for database to be available")
+        db_up = False
+        while  db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up=True
+            except (psycopg2_OP_error, OperationalError):
+                self.stdout.write("Database is unavailable ...waiting for 1 second")
+                time.sleep(1)
+        
+        self.stdout.write(self.style.SUCCESS('Database available!'))
+
