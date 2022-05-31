885ae13ea2e3cf17f6d7df04013bc639470fc324
aravinddps123 <aravinddps123@gmail.com>
2022-05-31 12:08:46 +0530

    Update calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,12 @@
 """ this is the file which has calculations modules"""
 
+
 def add(x, y):
     """ add two numbers"""
     return x + y
+
 
 def subtract(x, y):
     """ subtract x from y"""
 
     return y - x
-
-
-
