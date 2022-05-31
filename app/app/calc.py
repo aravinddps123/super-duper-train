54c043ec44b8c13049ff001c4d8caa9f3ad2b3e6
aravinddps123 <aravinddps123@gmail.com>
2022-05-31 09:33:32 +0530

    fixed urls

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+""" this is the file which has calculations modules"""
+
+def add(x, y):
+    """ add two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """ subtract x from y"""
+
+    return y-x
---
+++ app/app/test.py
@@ -0,0 +1,25 @@
+"""sample_ tests for application"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Test(SimpleTestCase):
+    """Test the calc module"""
+
+    
+    def test_add_number(self):
+        """Test the adding the number totgether"""
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+
+    
+    def test_subtract_number(self):
+        """Test the subtract the number totgether"""
+        res = calc.subtract(10, 16)
+        self.assertEqual(res, 6)
+
+
+
+
+
