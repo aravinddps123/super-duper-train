ef606eae1a7a8fbf438cc491af78699445853aca
aravinddps123 <aravinddps123@gmail.com>
2022-06-01 17:15:41 +0530

    Super user creation feature added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,51 @@
-from django.db import models # noqa
+"""
+Database Models for project
+"""
 
-# Create your models here.
+
+from django.db import models
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create , save, return a user"""
+
+        if not email:
+            raise ValueError("The user must have an email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create , save, return a superuser"""
+
+        user = self.create_user(email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the System"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
