50dbb831f607ae6ce0f492de133ab7058cdafd9d
aravinddps123 <aravinddps123@gmail.com>
2022-06-02 17:21:21 +0530

    linting issue corrected

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Test for the Django Admin """
 
@@ -14,17 +15,16 @@ class AdminSiteTests(TestCase):
         """Create a user and client we are creating two user and checking"""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = "admin@example.com",
-            password = "testpass123"
-            
+            email="admin@example.com",
+            password="testpass123",
         )
 
         self.client.force_login(self.admin_user)
 
         self.user = get_user_model().objects.create_user(
-            email = "user@example.com",
-            password = "testpass123",
-            name = "Test User",
+            email="user@example.com",
+            password="testpass123",
+            name="Test User",
         )
 
     def test_user_list(self):
@@ -34,10 +34,9 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-
     def test_edit_user_page(self):
         """Test the edit_user page works properly"""
-        url = reverse('admin:core_user_change', args= [self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
@@ -48,5 +47,3 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-
-
