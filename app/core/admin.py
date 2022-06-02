881087cf336ae9abe714b9ed90d41e9ffb749264
aravinddps123 <aravinddps123@gmail.com>
2022-06-02 17:06:35 +0530

    added drf

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin # noqa
+"""  
+Django admin customizations
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """FInd the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password', )}),
+        (
+            _('Permissions'),
+            {
+                "fields": (
+                    'is_staff',
+                    'is_active',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None,
+         {'classes': ('wide', ),
+          'fields':
+          {
+             'email',
+             'password',
+             'password1',
+             'name',
+             'is_staff',
+             'is_active',
+             'is_superuser'
+         }
+         }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
