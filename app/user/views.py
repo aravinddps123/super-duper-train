5e3708c778ce600f9dd78e594da647dd38ba4f01
aravinddps123 <aravinddps123@gmail.com>
2022-06-06 11:49:52 +0530

    Section 12 Complete

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """Views for the user   API"""
 
-from rest_framework import generics
+from rest_framework import generics , authentication, permissions
 from user.serializers import (UserSerializer,
 AuthTokenSerializer
 )
@@ -15,3 +15,17 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUsersView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user """
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """retrive and return the authenticated user"""
+        return self.request.user
+
+        
